795fe5549fc514fdedba72613ad84ddb0e159d82
shahid <shahdbest@gmail.com>
2023-02-23 12:24:53 +0530

    solved

--- src/main/java/com/stackroute/basics/RectanglePerimeter.java
+++ src/main/java/com/stackroute/basics/RectanglePerimeter.java
@@ -1,19 +1,28 @@
 package com.stackroute.basics;
 
-
+import java.util.Scanner;
 
 public class RectanglePerimeter {
     public static void main(String[] args) {
+//...
         new RectanglePerimeter().getValues();
+    
     }
 
     //get user input from console
     public void getValues() {
 
+    	Scanner sc=new Scanner(System.in);
+    	
+  double length=sc.nextDouble();
+  double breadth=sc.nextDouble();
+     	
+  double result= perimeterCalculator(length, breadth);
+    System.out.println(result);
     }
 
     //write logic to find perimeter of rectangle here
     public double perimeterCalculator(double length, double breadth) {
-        return 0;
+        return (double)( 2*(length+ breadth));
     }
 }
